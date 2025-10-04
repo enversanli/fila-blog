41cef3d729fb6944093ffc59538165ac36f76942
enversanli <en.sanli@hotmail.com>
2025-10-04 15:22:17 +0200

    access

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -55,6 +56,11 @@ class User extends Authenticatable
     }
 
     public function canAccessFilament(): bool
+    {
+        return true;
+    }
+
+    public function canAccessPanel(Panel $panel): bool
     {
         return true;
     }
